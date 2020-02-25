5a2b916bfcc8e408c4fd6e9b2716344da64843bc
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2020-02-25 15:34:06 +0300

    fix: array output

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@
 
 module.exports = function towelSort (matrix) {
     for (let i = 0; i < matrix.length; i++) {
-        if ((i + 1 ) % 2 === 0 ) {
+        if ((i + 1 ) % 2 == 0 ) {
             matrix[i].reverse();
+    }
+    }
+    let arr = [];
+    for (let i = 0 ; i < matrix.length; i++){
+        for (let j = 0; j < matrix[i].length; j++) {
+            arr.push(matrix[i][j])
         }
     }
-    return matrix.join(",")
+    return arr
 }
