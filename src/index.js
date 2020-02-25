f98153b70305bbf3bd3a541ab4cc592564d14542
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2020-02-25 15:40:45 +0300

    fix: output empty function parameter

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+    let arr = [];
+    if (matrix === undefined ) return arr;
     for (let i = 0; i < matrix.length; i++) {
         if ((i + 1 ) % 2 == 0 ) {
             matrix[i].reverse();
     }
     }
-    let arr = [];
     for (let i = 0 ; i < matrix.length; i++){
         for (let j = 0; j < matrix[i].length; j++) {
             arr.push(matrix[i][j])
